631cd8e39be49d29aecd6d3a438cbfa8decc4743
Ibrahim <kanoprogramm@gmail.com>
2025-09-01 22:43:31 +0500

    Add CancellationToken and update features

--- WebPagesDownloader/Program.cs
+++ WebPagesDownloader/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Threading;
 
 namespace WebPagesDownloader
 {
@@ -12,7 +14,8 @@ namespace WebPagesDownloader
         private static readonly HttpClient httpClient = new();
         static async Task Main(string[] args)
         {
-            List<string> urls = new() // List of URLs to download (you can put any websites here)
+            using CancellationTokenSource token = new();
+            List<string> urls = new()
             {
                 "https://www.google.com",
                 "https://www.youtube.com",
@@ -24,57 +27,79 @@ namespace WebPagesDownloader
                 "https://www.netflix.com",
                 "https://www.microsoft.com",
                 "https://www.apple.com"
-            };  // List of URLs to download (you can put any websites here)
+            };
+            Console.WriteLine("Starting async web page loading...");
+            Console.WriteLine("Press SPACEBAR to cancel loading\n");
 
-            Console.WriteLine("Starting async web page loading...\n");
+            var waitForEnterTask = WaitForEnterAsync(token.Token);
+            var downloadTask = DownloadAllUrlsAsync(urls, token.Token);
 
             try
             {
-                List<Task<WebPageResponse>> tasks = new();
-                foreach (var url in urls)
+                var completedTask = await Task.WhenAny(waitForEnterTask, downloadTask);
+
+                if (completedTask == waitForEnterTask)
                 {
-                    tasks.Add(GettingInformationAsync(url));
+                    Console.WriteLine("\nThe tasks were canceled!\n");
+                    token.Cancel();
+
+                    await Task.Delay(1000);
+                }
+                else
+                {
+                    Console.WriteLine("All tasks were successfully downloaded");
                 }
 
-                var results = await Task.WhenAll(tasks);
-
-                Console.WriteLine("\nLoading results:\n");
-                Console.WriteLine($"{"URL",-40} {"Characters",-15} {"Status",-15}");
-                Console.WriteLine(new string('-', 70));
-
-                foreach (var result in results)
-                {
-                    if (result.Success)
-                        Console.WriteLine($"{result.Url,-40} " +
-                            $"{result.CharCount,-15} {result.Success,-15}");
-                    else
-                    {
-                        Console.WriteLine($"{result.Url,-40} {"N/A",-15} {result.Success,-15}");
-                        Console.WriteLine($"    Error: [{result.ErrorMessage}]");
-                    }
-                }
-
-                int successfulCount = results.Count(x => x.Success);
-                int totalChars = results.Where(x => x.Success).Sum(u => u.CharCount);
-
-                Console.WriteLine(new string('-', 70));
-                Console.WriteLine($"Successfully loaded: {successfulCount} of {results.Length}");
-                Console.WriteLine($"Total characters: {totalChars}");
+                var results = await downloadTask;
+                if (results.Count > 0) BuildAndShowUrl(results);
+                else Console.WriteLine("No URLs were successfully loaded");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Critical error! {ex.Message}");
+                Console.WriteLine($"An error occurred: {ex.Message}");
             }
             finally
             {
                 httpClient.Dispose();
             }
-
-            Console.WriteLine("\nPress any key to exit...");
+            Console.WriteLine("\nPress any key to exit");
             Console.ReadKey();
             Console.Clear();
+
         }
-        private static async Task<WebPageResponse> GettingInformationAsync(string url)
+        private static async Task<List<WebPageResponse>> DownloadAllUrlsAsync(List<string> listOfUrls, CancellationToken cts)
+        {
+            var results = new List<WebPageResponse>();
+
+            foreach (var url in listOfUrls)
+            {
+                if (cts.IsCancellationRequested)
+                    break;
+
+                try
+                {
+                    await Task.Delay(500, cts);
+
+                    var result = await DownloadUrlAsync(url, cts);
+
+                    if (!result.Success)
+                    {
+                        Console.WriteLine($"[FAILED] {url} - {result.ErrorMessage}");
+                        continue;
+                    }
+
+                    results.Add(result);
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Operation was cancelled before starting next request");
+                    break;
+                }
+            }
+
+            return results;
+        }
+        private static async Task<WebPageResponse> DownloadUrlAsync(string url, CancellationToken cts)
         {
             try
             {
@@ -83,18 +108,14 @@ namespace WebPagesDownloader
                 if (!httpClient.DefaultRequestHeaders.Contains("User-Agent"))
                     httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
 
-                var response = await httpClient.GetAsync(url); response.EnsureSuccessStatusCode();
-
-                var content = await response.Content.ReadAsStringAsync();
-                int contentCount = content.Length;
-
-                Console.WriteLine($"Loaded: {url} - [{contentCount} characters]");
+                var response = await httpClient.GetStringAsync(url, cts);
+                if (string.IsNullOrEmpty(response)) Console.WriteLine("[Error] URL contains nothing");
 
                 return new WebPageResponse
                 {
                     Url = url,
-                    CharCount = contentCount,
-                    Success = true,
+                    CharCount = response.Length,
+                    Success = true
                 };
             }
             catch (HttpRequestException ex)
@@ -107,7 +128,7 @@ namespace WebPagesDownloader
                     ErrorMessage = $"HTTP Error: {ex.Message}"
                 };
             }
-            catch (TaskCanceledException ex)
+            catch (TaskCanceledException ex) when (ex.InnerException is TimeoutException)
             {
                 Console.WriteLine($"Timeout for {url}: {ex.Message}");
                 return new WebPageResponse
@@ -115,6 +136,16 @@ namespace WebPagesDownloader
                     Url = url,
                     Success = false,
                     ErrorMessage = $"Timeout: {ex.Message}"
+                };
+            }
+            catch (OperationCanceledException ex)
+            {
+                Console.WriteLine($"Operation cancelled for {url}");
+                return new WebPageResponse
+                {
+                    Url = url,
+                    Success = false,
+                    ErrorMessage = ex.Message
                 };
             }
             catch (Exception ex)
@@ -127,6 +158,38 @@ namespace WebPagesDownloader
                     ErrorMessage = $"Error: {ex.Message}"
                 };
             }
+        }
+        private static async Task WaitForEnterAsync(CancellationToken cts)
+        {
+            await Task.Run(() =>
+            {
+                while (!cts.IsCancellationRequested)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        var key = Console.ReadKey(true);
+                        if (ConsoleKey.Spacebar == key.Key)
+                        {
+                            break;
+                        }
+                    }
+                    Thread.Sleep(100);
+                }
+            }, cts);
+        }
+        private static void BuildAndShowUrl(List<WebPageResponse> results)
+        {
+            Console.WriteLine("\nLoading results...\n");
+            Console.WriteLine($"{"URL",-40} {"CharCount",-15} {"Success",-15}");
+            Console.WriteLine(new string('-', 70));
+
+            var successFullLoaded = results.Where(r => r.Success).Select(x => $"{x.Url,-40} {x.CharCount,-15} {x.Success,-15}");
+            Console.WriteLine(string.Join(Environment.NewLine, successFullLoaded));
+
+            var notSuccessFullLoaded = results.Where(r => !r.Success).Select(x => $"{x.Url,-40} {"-",-15} {x.Success,-15}");
+            Console.WriteLine(string.Join(Environment.NewLine, notSuccessFullLoaded));
+
+            Console.WriteLine(new string('-', 70));
         }
     }
     public class WebPageResponse
